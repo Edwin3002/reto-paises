fa258e35801fbffdb6b19872d2925d188780249a
Edwin <vargas11742@gmail.com>
2022-03-09 00:26:41 -0500

    tema oscuro

--- js/showData.js
+++ js/showData.js
@@ -2,19 +2,30 @@ export const showData = async(list,element) => {
     const products = await list;
     products.forEach(prod => {
         // const {name, alpha3code, capital, region, population, flags} = prod;
-        const {name, flags, region, cca3, population, capital} = prod;
+        const {name, flags, cca3, population, capital} = prod;
         element.innerHTML += `
-      <div class="card" style="width: 18rem;">
+      <div class="card bg-dark text-light" style="width: 18rem;">
         <img src="${flags.png}" class="card-img-top" alt="...">
         <div class="card-body">
           <h3>${name.common}</h3>
-          <p class="card-text">${population}</p>
-          <p>${region}</p>
-          <p>${capital}</p>
-          <p>${cca3}</p>
+          <p class="card-text">Población: ${population}</p>
+          <p>Continente: ${region}</p>
+          <p>Capital: ${capital}</p>
+          <p>Codigo: ${cca3}</p>
         </div>
       </div>
       `
     })
 }
 {/*  */}
+
+{/* <div class="card" style="width: 18rem;">
+        <img src="${flags.png}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h3>${name.common}</h3>
+          <p class="card-text">Población: ${population}</p>
+          <p>Continente: ${region}</p>
+          <p>Capital: ${capital}</p>
+          <p>Codigo${cca3}</p>
+        </div>
+      </div> */}
