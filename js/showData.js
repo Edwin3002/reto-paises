e827df260c19187233ded5c96da1748a54d852ad
Edwin <vargas11742@gmail.com>
2022-03-10 13:03:22 -0500

    last

--- js/showData.js
+++ js/showData.js
@@ -5,7 +5,7 @@ export const showData = async(list,element) => {
         // const {name, alpha3code, capital, region, population, flags} = prod;
         const {name, flags, cca3, population, capital, region} = prod;
         element.innerHTML += `
-      <div class="card bg-dark text-light" style="width: 18rem;">
+      <div class="card" style="width: 18rem;">
         <img src="${flags.png}" class="card-img-top" alt="...">
         <div class ="l">
           <p class = "pais">${name.common}</p>
