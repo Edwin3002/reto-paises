f02d3d3c41dd22bc68f2a584d347301550be4f5e
WendyColmenares <wjcolmenares@misena.edu.co>
2022-03-09 23:30:46 -0500

    Se agregan los estilos para el cambio de temas.

--- js/showData.js
+++ js/showData.js
@@ -5,17 +5,15 @@ export const showData = async(list,element) => {
         // const {name, alpha3code, capital, region, population, flags} = prod;
         const {name, flags, cca3, population, capital, region} = prod;
         element.innerHTML += `
-      <div class="card bg-dark text-light" style="width: 18rem;">
+      <div class="card">
         <img src="${flags.png}" class="card-img-top" alt="...">
-        <div class ="l">
-          <p class = "pais">${name.common}</p>
+          <p class = "nombre">${name.common}</p>
           <div class = "informacion">
           <p><strong>Population: </strong>${population}</p>
           <p><strong>Region: </strong>${region}</p>
           <p><strong>Capital: </strong>${capital}</p>
           <p><strong>Codigo: </strong>${cca3}</p>
           </div>
-        </div>
       </div>
       `
     })
